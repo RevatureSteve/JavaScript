b5685c988bc621629cb7a17f220be85200fad7c0
Steven Kelsey <steven.kelsey@revature.com>
2016-08-05 13:14:02 -0400

    jquery datatables

---
+++ JQueryDataTables/WebContent/myCoolDataTableScript.js
@@ -0,0 +1,22 @@
+/**
+ * The ready function ensures that the DOM is ready to execute JavaScript.
+ * Always put your jQuery inside of this function.
+ * 
+ */
+$(document).ready(function(){
+    
+	/*
+	 * Default settings
+	 */
+	
+    
+	
+	/*
+	 * Custom settings
+	 *  - Save the state of my DataTable
+	 *  - Disable ordering for column with index 1
+	 *   
+	 
+	
+    */
+});
